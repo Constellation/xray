d25b868e4324a7319b97a520efff3a36721d8186
Yusuke Suzuki <utatane.tea@gmail.com>
2013-12-11 05:09:24 +0900

    Initial implementation of executable with LLVM

---
+++ lib/core/main.cc
@@ -0,0 +1,19 @@
+#include <clang/AST/ASTConsumer.h>
+#include <clang/Driver/OptTable.h>
+#include <clang/Driver/Options.h>
+#include <clang/Frontend/ASTConsumers.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Rewrite/Frontend/FixItRewriter.h>
+#include <clang/Rewrite/Frontend/FrontendActions.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/Path.h>
+#include <llvm/Support/Signals.h>
+
+#include <iostream>
+
+int main(int argc, char** argv) {
+  std::cout << "HELLO" << std::endl;
+  return 0;
+}
